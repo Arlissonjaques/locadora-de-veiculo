bee2c74df430d250a513dff7b057f60411795693
Arlisson Jaques <arlissonjaques@gmail.com>
2020-09-17 19:30:44 -0500

    Inicio da interface

---
+++ LocadoraDeVeiculos/src/backend/Main.java
@@ -0,0 +1,12 @@
+package backend;
+import frontend.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Menu.menuPrincipal());
+        
+    }
+    
+}
---
+++ LocadoraDeVeiculos/src/frontend/Menu.java
@@ -0,0 +1,14 @@
+package frontend;
+
+public class Menu {
+
+    public static String menuPrincipal(){
+
+        return "1) - Fazer locação\n"
+        + "2) - Devoluçao do veiculo\n"
+        + "3) - Verificar locação\n"
+        + "4) - Fazer login\n"
+        + "0) - Sair";
+    }
+    
+}
